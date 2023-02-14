85810f6b7b17f446174171ed82bb07858ef1e03c
Jessica <jesscara.993@gmail.com>
2023-02-14 16:34:23 +0100

    models structured

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $fillable = [
+
+    'code',
+    'name',
+    'description',
+    'price',
+    'weight',
+  ];
+
+  public function categories()
+  {
+
+    return $this->belongsToMany(Category::class);
+  }
+  public function typology()
+  {
+
+    return $this->belongsTo(Typology::class);
+  }
 }
--- app/Models/Typology.php
+++ app/Models/Typology.php
@@ -7,5 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Typology extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $fillable = [
+
+    'code',
+    'name',
+    'digital',
+  ];
+
+  public function products()
+  {
+
+    return $this->hasMany(Product::class);
+  }
 }
