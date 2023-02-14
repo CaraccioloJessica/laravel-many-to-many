aaf906d575e2c96fe60e021e372485f78b6c816a
Jessica <jesscara.993@gmail.com>
2023-02-14 17:10:46 +0100

    homepage products by category

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layout.main-layout')
 
 @section('content')
-  <h1 class="text-center">Contenuto pagina</h1>
+    
+  <h1>Prodotti per categoria</h1>
+    @foreach ($categories as $category)
+      <h2 class="text-primary">Categoria: {{ $category -> name }}</h2>
+      <ul>
+        @foreach ($category -> products as $product)
+          <li>
+            <h4>Prodotto</h4>
+            [{{ $product -> code }}] {{ $product -> name }}
+            - {{ $product -> typology -> name }}
+            - DIGITAL: 
+            {{ $product -> typology -> digital ? "YES" : "NO" }}
+          </li>
+        @endforeach
+      </ul>
+    @endforeach
+
 @endsection
