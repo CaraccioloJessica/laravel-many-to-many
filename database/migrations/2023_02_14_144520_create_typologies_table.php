52d373b416c17ea9235ef77649625294d577cb9c
Jessica <jesscara.993@gmail.com>
2023-02-14 16:07:28 +0100

    filled migrations table

--- database/migrations/2023_02_14_144520_create_typologies_table.php
+++ database/migrations/2023_02_14_144520_create_typologies_table.php
@@ -4,28 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('typologies', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('typologies', function (Blueprint $table) {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('typologies');
-    }
+      $table->id();
+
+      $table->string('code', 5)->unique();
+      $table->string('name', 64);
+      $table->boolean('digital')->default(false);
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('typologies');
+  }
 };
